78eebea3413d29007aef9593940dc8faac7453fa
Matteo Soresini <matteo.soresini@hotmail.it>
2026-02-26 09:52:21 +0100

    reanimated

--- app/tabs/native-modules/reanimated.tsx
+++ app/tabs/native-modules/reanimated.tsx
@@ -1,11 +1,17 @@
+import { BouncyBox } from "@/components/reanimated/bouncy-box";
+import { FadeSlideCard } from "@/components/reanimated/fade-slice-card";
 import { ProgressBarTap } from "@/components/reanimated/progress-bar-tap";
 import { View } from "react-native";
 
 export function Reanimated() {
   return (
-    <View>
-      {/* <FadeSlideCard />
-      <BouncyBox /> */}
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "space-between",
+      }}>
+      <FadeSlideCard />
+      <BouncyBox />
       <ProgressBarTap />
     </View>
   );
