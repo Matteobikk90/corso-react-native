9bbd2c043e1a4fb5fe0a3458506263bb90634bbe
Matteo Soresini <matteo.soresini@hotmail.it>
2026-02-26 13:17:40 +0100

    expo dev client + expo go notifications

--- app/tabs/native-modules/notifications-wrapper/index.tsx
+++ app/tabs/native-modules/notifications-wrapper/index.tsx
@@ -1,10 +1,10 @@
 import { View } from "react-native";
-import { ExpoNotificationsLocal } from "./expo-notifications-local";
+import { ExpoNotificationsDevClient } from "./expo-notifications-dev-client";
 
 export function NotificationsWrapper() {
   return (
     <View style={{ padding: 16, gap: 12 }}>
-      <ExpoNotificationsLocal />
+      <ExpoNotificationsDevClient />
     </View>
   );
 }
